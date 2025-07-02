c757b8151614b8d05348a4b006bcc1968623a7a1
Claudia Sofía Pocasangre Peralta <00005225@uca.edu.sv>
2025-07-02 12:48:50 -0600

    Estructura basica del proyecto

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,3 +6,15 @@ void GetConsoleSize(int &rows, int &columns) {
     columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
     rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 }
+
+void PrintCentered(const vector<string>& content) {
+    int rows, columns;
+    GetConsoleSize(rows, columns); 
+
+    for (const string &line : content) {
+        int padding = (columns - (int)line.length()) / 2;
+        if (padding < 0) padding = 0;
+
+        cout << string(padding, ' ') << line << endl;
+    }
+}
