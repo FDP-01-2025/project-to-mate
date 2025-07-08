c16e7c1b7a303d20114df079325856185fc3532b
Claudia Sofía Pocasangre Peralta <00005225@uca.edu.sv>
2025-07-07 21:44:22 -0600

    basic project structure

--- src/game.cpp
+++ src/game.cpp
@@ -1,17 +1,22 @@
 #include "GameStartMenu.h"
 #include "dialogues.h"
-#include "BattleInterface.h"
+#include "battle.h"
+bool no_repetir = true;
+
 
 void GameStart() {
+    if(no_repetir){
     RunCharacterMenu();
+    }
     
     vector<vector<string>> FightBattles = {
         {"FirstBattle.txt", "FirstBattleDefeat.txt", "FirstBattleWin.txt", "FirstBattleWin.txt"},
-        {"SecondBattle.txt", "SecondBattleDefeat.txt", "SecondBattleWin.txt", "SecondBattleWin"},
-        {"FinalBoss.txt", "FinalBossDefeat.txt", "FinalBossNormalWin.txt, FinalBossGoodWin.txt"},
+        {"SecondBattle.txt", "SecondBattleDefeat.txt", "SecondBattleWin.txt", "SecondBattleWin.txt"},
+        {"LeaguePresident.txt", "LeaguePresidentDefeat.txt", "LeaguePresidentWin.txt", "LeaguePresidentWin.txt"},
         {"SecretBoss.txt", "SecretBossDefeat.txt", "SecretBossNormalWin.txt", "SecretBossGoodWin.txt"},
-        {"LeguePresident.txt", "LeaguePresidentDefeat.txt", "LeaguePresidentWin.txt", "LeguePresidentWin.cpp"}
+        {"FinalBoss.txt", "FinalBossDefeat.txt", "FinalBossNormalWin.txt", "FinalBossGoodWin.txt"}
     };
+
     
     bool Defeat = false, NormalWin = false, GoodWin = false;
     //Battle basic structure
@@ -20,35 +25,97 @@ void GameStart() {
         PrintBackgroundDialogue("TowerBackground.txt", TowerColorID, FightBattles[battle][0]);
         
         //FIGHT LOOKS
+        bool siguen = true;
+        float modificador = 0;
+        float Enemy_modificador = 0;
+        int maxTurns=0;
+        int CantObjetos[4] = {3, 3, 3, 3};
+        SivarmonDataBase Jugador = SivarmonesCall(SelecterCharacter);
+        SivarmonDataBase Enemy = SivarmonesCall(IdEnemy1);
+        int VidaOriginal1 = Jugador.vida;
         PrintBattleEnemies(SelecterCharacter, IdEnemy1);
-        MovesMenu(SelecterCharacter, IdEnemy1);
+        int Seleccion;
+       do
+       {
+        bool IsMove;
+        if(maxTurns>20){
+            NormalWin=1;
+            Defeat=0;
+            GoodWin=0;
+            break;
+        }
+        maxTurns++;
+        MovesMenu(SelecterCharacter, IdEnemy1, IsMove, Seleccion );
+        // cout<<Seleccion << "HOla";
+        
+        Battlebucle(Seleccion, siguen, IsMove, modificador, Enemy_modificador,CantObjetos,
+        Jugador, Enemy, Defeat, NormalWin, GoodWin, VidaOriginal1);
+
+       } while (siguen && maxTurns<=19);
+       
+        
         
         //HERE GOES THE BATTLE CHANGES
 
         if(Defeat){
-            PrintBackgroundDialogue("YouWin.txt", YourWinColorID, FightBattles[battle][1]);
+            system("cls");
+            PrintBackgroundDialogue("YouWinBackground.txt", YourWinColorID, FightBattles[battle][1]);
+            sleep_for(0.1s);
             return; //Go back to the main menu
         }
         else if(NormalWin){
-            PrintBackgroundDialogue("Paradise.txt", ParadiseColorID, FightBattles[battle][2]);
+            PrintBackgroundDialogue("ParadiseBackground.txt", ParadiseColorID, FightBattles[battle][2]);
+            no_repetir= false;
         }
         else{
-            PrintBackgroundDialogue("Paradise.txt", TowerColorID, FightBattles[battle][3]);
+            PrintBackgroundDialogue("ParadiseBackground.txt", ParadiseColorID, FightBattles[battle][3]);
+            no_repetir=false;
         }
+        sleep_for(0.1s);
     }
     
 }
 
 void About() {
-    cout << "How to play?\n\tAfter selecting a character, you will face game battles\nwhere you can attack, defend or just leave.\n";
+    system("cls");
+    int rows, cols;
+    GetConsoleSize(rows, cols);
+    PrintSivarMesagge(GameName);
+    cout<<endl;
+    string aboutG= "ABOUT THE GAME";
+    cout<<string(cols/2 - aboutG.size()/2, ' ') << FG_ORANGE << aboutG << RESET << endl << endl;
+    vector<string> GameExplanation = {
+        "Welcome to the world of Sivarmons!",
+        "Once you choose your Sivarmon, your journey begins",
+        "You will face three intense battles, each more challenging than the last",
+        "In each fight, you can use powerful attacks or helpful items",
+        "Choose your strategy wisely — your victory depends on it!",
+        "If you win all three battles... something unexpected might happen",
+        "A mysterious villain may reveal themselves when you least expect it",
+        "Will you be strong enough to face the ultimate challenge?"
+    };
+    for(string line: GameExplanation){
+        cout<<string(cols/2 - line.size()/2, ' ');
+        cout<<line<<endl;
+    }
     getch();
 }
 
 void Credits() {
-    cout << BG_MAGENTA << FG_BLACK << NEGRITA << "THIS GAME EXISTS THANKS TO:\n" << RESET;
-    cout << "Diego Armando Mata Cortez [00115025]\n";
-    cout << "Manuel Tobar Garcia [00067423]\n";
-    cout << "Andy Samuel Ochoa Gonzalez [00064825]\n";
-    cout << "Claudia Sofia Pocasangre Peralta [00005225]\n";
+    int rows, cols;
+    GetConsoleSize(rows, cols);
+    PrintSivarMesagge(GameName);
+    for(int i=0;i<3;i++) cout<<endl;
+    cout << string((cols/2)-(27/2), ' ') << BG_BLACK << FG_ORANGE << NEGRITA << "THIS GAME EXISTS THANKS TO" << RESET << endl;
+    for(int i=0;i<2;i++) cout<<endl;
+    string name1 = "Diego Armando Mata Cortez [00115025]\n";
+    string name2 = "Manuel Tobar Garcia [00067423]\n";
+    string name3 = "Andy Samuel Ochoa Gonzalez [00064825]\n";
+    string name4 = "Claudia Sofia Pocasangre Peralta [00005225]\n";
+
+    cout << string((cols)/2 - (name1.size())/2, ' ') << name1 <<endl;
+    cout << string((cols)/2 - (name2.size())/2, ' ') << name2 <<endl;
+    cout << string((cols)/2 - (name3.size())/2, ' ') << name3 <<endl;
+    cout << string((cols)/2 - (name4.size())/2, ' ') << name4 <<endl;
     getch();
 }
