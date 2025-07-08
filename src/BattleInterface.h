111379ba772ab8d0dbeb45f593a851d51ac25844
Claudia Sofía Pocasangre Peralta <00005225@uca.edu.sv>
2025-07-07 23:50:30 -0600

    battles logic funtions

--- src/BattleInterface.h
+++ src/BattleInterface.h
@@ -10,12 +10,12 @@ void PrintBattleEnemies(int SivarmonSelected, int Enemy){
     drawBackgroundWithOvals();  
     SpritesDataBase OP_SPRITE = SpritesCall(SivarmonSelected);
     SpritesDataBase CPU_SPRITE = SpritesCall(Enemy);
-
+    //call the sprite from the data base
     SetConsoleOutputCP(CP_UTF8);
-
+    //use this funtion to make all character avaliables
     int consoleRows, consoleCols;
     GetConsoleSize(consoleRows, consoleCols);
-
+    //look for the maximum row on both sprites
     int maxLeftWidth = 0, maxRightWidth = 0;
     for (const auto& line : OP_SPRITE.Sprite){
         maxLeftWidth = max(maxLeftWidth, (int)line.size());
@@ -30,29 +30,28 @@ void PrintBattleEnemies(int SivarmonSelected, int Enemy){
     int c2_x = c1_x;             
     int c2_y = 3 * consoleCols / 4;
     int centerY = half + half / 2 - 7;
-
+    //set new variables for the sprites positions
     int centerX1 = consoleCols / 3;
     int ovalRightStartX = c2_y; 
 
     int centerX2 = ovalRightStartX;
-
-    int offsetY = -1;
+    //offsets are to move vertical and horizontal between distances for the sprites
+    int offsetY = -1; 
     int offsetX = 10; 
 
-    int sprite1_height = OP_SPRITE.Sprite.size();
+    int sprite1_height = OP_SPRITE.Sprite.size(); // to get both sprtites height
     int sprite2_height = CPU_SPRITE.Sprite.size();
 
     int leftStartX = centerX1 - maxLeftWidth / 2 + offsetX;
     int leftStartY = centerY - sprite1_height + 1 + offsetY;
 
-    int spacing = 5; 
+    int spacing = 5; //spacing between sprites
     int rightStartX = leftStartX + maxLeftWidth + spacing;
-
     int rightStartY = centerY - sprite2_height + 1 + offsetY;
-
+    //coordenates to start printing
     int lines = max(sprite1_height, sprite2_height);
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
+    //a for util the longest line to make the sprites alight from the bottom
     for (int i = 0; i < lines; i++) {
         if (i < sprite1_height) {
             const string& line = OP_SPRITE.Sprite[i];
@@ -60,12 +59,13 @@ void PrintBattleEnemies(int SivarmonSelected, int Enemy){
             SetConsoleCursorPosition(hConsole, pos);
             for (char ch : line) {
                 if (ch != ' ') {
-                    cout << ch;
+                    cout << ch; //if is not an empty space just print it
                 } else {
-                    cout << "\033[C";
+                    cout << "\033[C"; //move the cursor to the next space
                 }
             }
         }
+        //an move the cursor each of the
 
         if (i < sprite2_height) {
             const string& line = CPU_SPRITE.Sprite[i];
@@ -73,9 +73,9 @@ void PrintBattleEnemies(int SivarmonSelected, int Enemy){
             SetConsoleCursorPosition(hConsole, pos);
             for (char ch : line) {
                 if (ch != ' ') {
-                    cout << ch;
+                    cout << ch; //same as before
                 } else {
-                    cout << "\033[C";
+                    cout << "\033[C"; //same as before
                 }
             }
         }
@@ -89,14 +89,14 @@ void PrintBattleEnemies(int SivarmonSelected, int Enemy){
 int PrintSelectionMenu(const vector<string>& names, const vector<string>& typeNames, int boxLeftCol, int boxTopRow, int boxWidth) {
     int consoleRows, consoleCols;
     GetConsoleSize(consoleRows, consoleCols);
-
+    //print the white dialog
     int dialogueHeight = 10;
     int selectedIndex = 0;
     int startCol = boxLeftCol + 4;
     int startRow = boxTopRow + 2;
     int colSpacing = 25;
     int rowSpacing = 3;
-
+    //show the menu options
     auto printOptions = [&]() {
         for (int i = 0; i < names.size(); i++) {
             int row = startRow + (i / 2) * rowSpacing;
@@ -117,7 +117,7 @@ int PrintSelectionMenu(const vector<string>& names, const vector<string>& typeNa
     printOptions();
 
     while (true) {
-        // Imprimir descripción del tipo a la derecha
+        //print the descriptions to the right
         int typeBoxCol = boxLeftCol + boxWidth + 60;
         if (typeBoxCol + 30 >= consoleCols) {
             typeBoxCol = consoleCols - 30;
@@ -151,19 +151,20 @@ int PrintSelectionMenu(const vector<string>& names, const vector<string>& typeNa
 
 
 void MovesMenu(int SivarmonSelected, int Enemy, bool& que, int& seleccion) {
+    //show the moves menu
     TiposDataBase OP_TYPES = TiposCall(SivarmonSelected);
     MovimientosDataBase OP_MOVES = MovimientosCall(SivarmonSelected);
     SivarmonDataBase OP_INFO = SivarmonesCall(SivarmonSelected);
     int consoleRows, consoleCols;
     GetConsoleSize(consoleRows, consoleCols);
-
+    //call all the files needed
     int dialogueHeight = 10;
     int boxTopRow = bottomline + 7; 
     int optionBoxWidth = 20;
     int maxLineLength = consoleCols - 35;
     int totalBoxWidth = maxLineLength + optionBoxWidth;
     int boxLeftCol = (consoleCols - totalBoxWidth) / 2;
-
+    //show the background
     for(int i = 0; i < dialogueHeight; i++){
         cout << "    " << RESET << "\033[" << (boxTopRow + i) << ";" << boxLeftCol << "H";
         cout << RESET << BG_WHITE << string(maxLineLength, ' ') << RESET;
@@ -184,7 +185,7 @@ void MovesMenu(int SivarmonSelected, int Enemy, bool& que, int& seleccion) {
 
     int optionStartCol = optionBoxCol;
     int optionStartRow = boxTopRow;
-
+    //show the options
     auto printOptions = [&]() {
         for (int i = 0; i < dialogueHeight; i++) {
             cout << "\033[" << (optionStartRow + i) << ";" << optionStartCol << "H";
@@ -217,7 +218,7 @@ void MovesMenu(int SivarmonSelected, int Enemy, bool& que, int& seleccion) {
         }
         printOptions();
     }
-    
+    //reset all by printing again
     for(int i = 0; i < dialogueHeight; i++){
        cout << "    " << RESET << "\033[" << (boxTopRow + i) << ";" << boxLeftCol << "H";
        cout << RESET << BG_WHITE << string(maxLineLength, ' ') << RESET;
@@ -232,7 +233,7 @@ void MovesMenu(int SivarmonSelected, int Enemy, bool& que, int& seleccion) {
     int moveBoxTopRow = boxTopRow;
     int moveBoxWidth = maxLineLength;
 
-    if (selected == 0) { 
+    if (selected == 0) { //if its a move selected
         vector<string> moveNames;
         vector<string> moveTypeNames;
 
@@ -249,7 +250,7 @@ void MovesMenu(int SivarmonSelected, int Enemy, bool& que, int& seleccion) {
         que = true;
 
     }
-    else {
+    else { //if an object is selected
         int ObjectsID[4] = {1, 2, 3, 4};
         vector<string> ObjectNames;
         vector<string> ObjectDamage;
